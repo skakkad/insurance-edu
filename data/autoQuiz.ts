688b48541ea6e0b3a4065a1e5190819a79a8ae35
shreyahsmn <141703497+shreyahsmn@users.noreply.github.com>
2023-09-24 11:43:47 -0500

    progress bar

---
+++ data/autoQuiz.ts
@@ -0,0 +1,18 @@
+// autoQuiz.js
+
+const autoQuiz = [
+    {
+      question: 'What is the capital of France?',
+      options: ['London', 'Madrid', 'Paris', 'Berlin'],
+      correctAnswer: 'Paris',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Earth', 'Mars', 'Jupiter', 'Saturn'],
+      correctAnswer: 'Mars',
+    },
+    // Add more questions and answers here
+  ];
+  
+  export default autoQuiz;
+  
